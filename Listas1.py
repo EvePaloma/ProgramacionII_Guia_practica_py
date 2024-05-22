fbea3e8561fab7e599c4ee1a60ab21204c4770ec
Evelyn Paloma <eve.paloma92e@mail.com>
2024-05-22 19:23:36 -0300

    Suma de elementos

---
+++ Listas1.py
@@ -0,0 +1,12 @@
+'''
+1. Suma de elementos: Escribe una función que tome una lista de números y devuelva la
+suma de todos los elementos en la lista.
+'''
+
+def suma_de_elementos(lista):
+
+    return sum(lista)
+
+num = [1,2,3,10,50,34]
+resultado = suma_de_elementos(num)
+print(resultado)
