889251ae8bbdf38839e7cfada92acbaf56c2abb5
Evelyn Paloma <eve.paloma92e@mail.com>
2024-05-26 18:30:57 -0300

    Diccionario de cuadrados

---
+++ Diccionarios5.py
@@ -0,0 +1,16 @@
+'''
+Diccionario de cuadrados: Escribe una función que reciba un número n y devuelva un
+diccionario con los números de 1 a n como claves y sus cuadrados como valores.
+'''
+
+def diccionario_de_cuadrados(n):
+    diccionario = {}
+
+    for i in range(1,n +1):
+        diccionario[i] = i**2
+
+    return diccionario
+
+n = 10
+resultado = diccionario_de_cuadrados(n)
+print(f"Los números son: {resultado}")    
